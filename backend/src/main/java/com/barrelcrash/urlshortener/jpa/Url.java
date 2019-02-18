3cee5e12091c74aec7b2233aa7b065620c37f9c1
Luke Abel <luke.abel@gmail.com>
2019-02-18 06:53:01 -0500

    backend: adding timestamp to table

--- backend/src/main/java/com/barrelcrash/urlshortener/jpa/Url.java
+++ backend/src/main/java/com/barrelcrash/urlshortener/jpa/Url.java
@@ -1,11 +1,14 @@
 package com.barrelcrash.urlshortener.jpa;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 
 import com.barrelcrash.urlshortener.util.UrlConversionUtil;
@@ -22,9 +25,16 @@ public class Url {
 	@Column(name = "ORIGIN_URL", nullable = false)
 	private String originUrl;
 	
-	@Column(name = "SHORT_URL", nullable = false)
+	@Column(name = "SHORT_URL")
 	private String shortUrl;
+
+	@Column(name = "CREATED", nullable = false)
+	private Date createdDate;
 	
+	@PrePersist
+	protected void onCreate() {
+		this.createdDate = new Date();
+	}
 	
 	public Long getId() {
 		return this.id;
